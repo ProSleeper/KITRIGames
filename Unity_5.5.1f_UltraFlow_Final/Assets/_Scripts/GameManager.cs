f77952a3bbbe50b5cf0171436fdd2848f2b8234f
ProSleeper <ingngnin@gmail.com>
2017-06-13 23:47:38 +0900

    Unity_5.5.1f_UltraFlow_Final
    키트리 울트라플로우 최종본

---
+++ Unity_5.5.1f_UltraFlow_Final/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum StageScene
+{
+	None,
+	Stage_01,
+	Max
+}
+
+public class GameManager : MonoBehaviour
+ {
+	StageScene CurStage = StageScene.None;
+
+	void Start ()
+	{
+		// 4.X 5.X 초반
+		// Application.LoadLevel(0);
+		//
+		// SceneManager.LoadScene()
+	}
+	
+	void Update ()
+	{
+		// Backspave -> 안드로이드 취소키
+		if (Input.GetKeyDown(KeyCode.Backspace))
+		{
+			CurStage++;
+			if (CurStage >= StageScene.Max)
+			{
+				CurStage = StageScene.Max;
+				return;
+			}		
+
+			SceneManager.LoadScene(
+				CurStage.ToString());
+		}
+	}
+}
---
+++ Unity_5.5.1f_UltraFlow_Final/Assets/_Scripts/GoalObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalObject : MonoBehaviour
+{
+    Transform BallTrans = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Contains("BounceBall"))
+        {
+            BallTrans = collision.transform;
+            collision.GetComponent<Rigidbody2D>().velocity =
+                Vector2.zero;
+
+            GameObject.
+                Find("Stage").
+                GetComponent<Stage>().
+                IsGameOver(true);
+
+        }
+    }
+
+    private void Update()
+    {
+        if(BallTrans != null)
+        {
+            BallTrans.position
+                = Vector2.Lerp(BallTrans.position,
+                this.transform.position, 3f * Time.deltaTime);
+        }
+    }
+
+}
